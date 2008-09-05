88b6a696b81307011fc58bada270d7d7ab98b628
David Yong <david.young@ctl.io>
2008-09-05 16:44:29 +0000

    VER 42432: changed shell process to asynchronous

--- src/com/savvis/it/tools/web/servlet/GenericUploadServlet.java
+++ src/com/savvis/it/tools/web/servlet/GenericUploadServlet.java
@@ -43,11 +43,11 @@ import com.savvis.it.util.*;
  * This class handles the home page functionality 
  * 
  * @author David R Young
- * @version $Id: GenericUploadServlet.java,v 1.21 2008/09/03 17:06:29 telrick Exp $
+ * @version $Id: GenericUploadServlet.java,v 1.22 2008/09/05 16:44:29 dyoung Exp $
  */
 public class GenericUploadServlet extends SavvisServlet {	
 	private static Logger logger = Logger.getLogger(GenericUploadServlet.class);
-	private static String scVersion = "$Header: /opt/devel/cvsroot/SAVVISRoot/CRM/tools/java/Web/src/com/savvis/it/tools/web/servlet/GenericUploadServlet.java,v 1.21 2008/09/03 17:06:29 telrick Exp $";
+	private static String scVersion = "$Header: /opt/devel/cvsroot/SAVVISRoot/CRM/tools/java/Web/src/com/savvis/it/tools/web/servlet/GenericUploadServlet.java,v 1.22 2008/09/05 16:44:29 dyoung Exp $";
 	
 	private static PropertyManager properties = new PropertyManager("/properties/genericUpload.properties");
 	
@@ -226,7 +226,7 @@ public class GenericUploadServlet extends SavvisServlet {
 							Map<String, Object> cmdMap = commands.get(i);
 							String cmd = inputsContext.keywordSubstitute(cmdMap.get("cmdString") + " " + cmdMap.get("argString"));
 							logger.info("cmd: " + cmd);
-//							clp.setWaitForProcess(false);
+							clp.setWaitForProcess(false);
 							clp.setDir(new File((String) cmdMap.get("startDir")));
 							Context envContext = new Context();
 							envContext.fillWithEnvAndSystemProperties();
