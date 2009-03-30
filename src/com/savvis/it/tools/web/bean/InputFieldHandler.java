c7492dde45e350a222e6bb79a4f14a410a990eef
David Yong <david.young@ctl.io>
2009-03-30 18:09:32 +0000

    VER 45988: added additional input types, added regex

--- src/com/savvis/it/tools/web/bean/InputFieldHandler.java
+++ src/com/savvis/it/tools/web/bean/InputFieldHandler.java
@@ -3,9 +3,11 @@
  */
 package com.savvis.it.tools.web.bean;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,21 +15,20 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.w3c.dom.NodeList;
 
-import com.savvis.it.db.*;
+import com.savvis.it.db.DBUtil;
 import com.savvis.it.util.ObjectUtil;
 import com.savvis.it.util.SimpleNode;
 import com.savvis.it.util.XmlUtil;
-import com.sun.corba.se.impl.orbutil.closure.Constant;
 
 /**
  * This class handles the processing and creation of generic inputs for web pages. 
  * 
  * @author David R Young
- * @version $Id: InputFieldHandler.java,v 1.7 2008/09/17 18:33:03 dyoung Exp $
+ * @version $Id: InputFieldHandler.java,v 1.8 2009/03/30 18:09:32 dyoung Exp $
  */
 public class InputFieldHandler {	
 	private static Logger logger = Logger.getLogger(InputFieldHandler.class);
-	private static String scVersion = "$Header: /opt/devel/cvsroot/SAVVISRoot/CRM/tools/java/Web/src/com/savvis/it/tools/web/bean/Attic/InputFieldHandler.java,v 1.7 2008/09/17 18:33:03 dyoung Exp $";
+	private static String scVersion = "$Header: /opt/devel/cvsroot/SAVVISRoot/CRM/tools/java/Web/src/com/savvis/it/tools/web/bean/Attic/InputFieldHandler.java,v 1.8 2009/03/30 18:09:32 dyoung Exp $";
 	
 	/*
 	 * Valid types of inputs:
@@ -46,9 +47,15 @@ public class InputFieldHandler {
 	
 	private Boolean required;
 	private Boolean readonly;
+	private String regex;
+	private String regexValidationText;
+	private String format;
+	private int maxlength;
 
 	private static final String INPUT_TYPE_SELECT = "select";
 	private static final String INPUT_TYPE_SQLSELECT = "sqlselect";
+	private static final String INPUT_TYPE_FILESELECT = "fileselect";
+	private static final String INPUT_TYPE_CFGSELECT = "cfgselect";
 	private static final String INPUT_TYPE_TEXT = "input";
 	private static final String INPUT_TYPE_DATE = "date";
 	private static final String INPUT_TYPE_NUMERIC = "numeric";
@@ -84,6 +91,28 @@ public class InputFieldHandler {
 					input.setReadonly(false);
 				}
 			}
+			
+			// regexp
+			if (!ObjectUtil.isEmpty(inputNode.getTextContent("regEx"))) {
+				input.setRegex(inputNode.getTextContent("regEx"));
+				
+				logger.info("inputNode.getTextContent(\"regExText\"): " + inputNode.getTextContent("regExText"));
+				if (!ObjectUtil.isEmpty(inputNode.getTextContent("regExText"))) {
+					input.setRegexValidationText(inputNode.getTextContent("regExText"));
+				}
+			}
+
+			if (!ObjectUtil.isEmpty(inputNode.getAttribute("format"))) {
+				input.setFormat(inputNode.getAttribute("format"));
+			}
+			if (!ObjectUtil.isEmpty(inputNode.getAttribute("maxlength"))) {
+				try {
+					Integer maxlen = Integer.parseInt(inputNode.getAttribute("maxlength"));
+					input.setMaxlength(maxlen);
+				} catch (Exception e) {
+					throw new Exception("maxlength must be an integer");
+				}
+			}
 
 			input.setName(inputNode.getTextContent("{name}"));
 			input.setType(inputNode.getTextContent("{type}"));
@@ -109,11 +138,31 @@ public class InputFieldHandler {
 			// we will retrieve values from a database during the "get" method, only validation happens here
 			if (INPUT_TYPE_SQLSELECT.equals(input.getType().toLowerCase())) {
 				if (!ObjectUtil.isEmpty(inputNode.getSimpleNode("{values_sql}"))) {
-					String dbDriver = inputNode.getSimpleNode("{values_sql}").getAttribute("dbdriver");
-					
 					if (ObjectUtil.isEmpty(inputNode.getSimpleNode("{values_sql}").getAttribute("valueCol")))
 						throw new Exception("valueCol is required for values_sql");
+				} else {
+					throw new Exception("values_sql is required for input type sqlselect");
+				}
+			}
+
+			// we will retrieve values from filesystem during the "get" method, only validation happens here
+			if (INPUT_TYPE_FILESELECT.equals(input.getType().toLowerCase())) {
+				if (ObjectUtil.isEmpty(inputNode.getSimpleNode("{values_dir}"))) {
+					throw new Exception("values_dir is required for input type fileselect");
 				}				
+			}
+
+			// we will retrieve values from cfg during the "get" method, only validation happens here
+			if (INPUT_TYPE_CFGSELECT.equals(input.getType().toLowerCase())) {
+				if (ObjectUtil.isEmpty(inputNode.getSimpleNode("{values_cfg}"))) {
+					throw new Exception("values_cfg is required for input type cfgselect");
+				} else {
+					// validate cfg exists
+					File cfgFile = new File(inputNode.getTextContent("{values_cfg}"));
+					if (!cfgFile.exists()) {
+						throw new Exception ("config file in values_cfg doesn't exist (" + cfgFile + ")");
+					}
+				}
 			}
 
 			inputsMap.put(input.getName(), input);
@@ -146,25 +195,72 @@ public class InputFieldHandler {
 	}
 
 	public List<String> getValues() {
+		logger.info("this.type: " + this.type);
 		
 		try {
-			if (ObjectUtil.isEmpty(this.values) && INPUT_TYPE_SQLSELECT.equals(this.type.toLowerCase())) {
-				if (!ObjectUtil.isEmpty(this.node.getNode("{values_sql}"))) {
-					String dbDriver = this.node.getSimpleNode("{values_sql}").getAttribute("dbdriver");
-					String valueCol = this.node.getSimpleNode("{values_sql}").getAttribute("valueCol");
-					String sql = this.node.getTextContent("{values_sql}");
-					
-					DBUtil.setEnableKeywordSubstitution(true);
-					List results = DBUtil.executeQuery(dbDriver, sql);
-					if (results.size() > 0) {
-						List<String> values = new ArrayList<String>();
-						for (int j = 0; j < results.size(); j++) {
-							Map result = (Map) results.get(j);
-							values.add(result.get(valueCol).toString());
+			if (ObjectUtil.isEmpty(this.values)) {
+				
+				if (INPUT_TYPE_SQLSELECT.equals(this.type.toLowerCase())) {
+					if (!ObjectUtil.isEmpty(this.node.getNode("{values_sql}"))) {
+						String dbDriver = this.node.getSimpleNode("{values_sql}").getAttribute("dbdriver");
+						String valueCol = this.node.getSimpleNode("{values_sql}").getAttribute("valueCol");
+						String sql = this.node.getTextContent("{values_sql}");
+						
+						DBUtil.setEnableKeywordSubstitution(true);
+						List results = DBUtil.executeQuery(dbDriver, sql);
+						if (results.size() > 0) {
+							List<String> values = new ArrayList<String>();
+							for (int j = 0; j < results.size(); j++) {
+								Map result = (Map) results.get(j);
+								values.add(result.get(valueCol).toString());
+							}
+							this.values = values;
 						}
-						this.values = values;
 					}
-				}				
+				}
+				
+				if (INPUT_TYPE_FILESELECT.equals(this.type.toLowerCase())) {
+					if (!ObjectUtil.isEmpty(this.node.getNode("{values_dir}"))) {
+						File dir = new File(this.node.getTextContent("{values_dir}"));
+						logger.info("dir: " + dir);
+						File[] fileList = dir.listFiles();
+						logger.info("fileList.length: " + fileList.length);
+						if (fileList.length > 0) {
+							List<String> values = new ArrayList<String>();
+							for (int j = 0; j < fileList.length; j++) {
+								File f = fileList[j];
+								values.add(f.getName());
+							}
+							Collections.sort(values);
+							this.values = values;
+						}
+					}				
+				}
+				
+				if (INPUT_TYPE_CFGSELECT.equals(this.type.toLowerCase())) {
+					if (!ObjectUtil.isEmpty(this.node.getNode("{values_cfg}"))) {
+						File cfg = new File(this.node.getTextContent("{values_cfg}"));
+						
+						if (cfg.exists()) {
+							List<String> values = new ArrayList<String>();
+							
+							FileReader reader = new FileReader(cfg);
+							BufferedReader br = new BufferedReader(reader);
+							String s;
+							int lineCnt = 0;
+							while((s = br.readLine()) != null) { 
+								if (s.startsWith("#"))
+									continue;
+								values.add(s);
+							} 
+							reader.close();
+							
+							Collections.sort(values);
+							this.values = values;
+						}
+					}				
+				}
+
 			}
 		} catch (Exception e) {
 			logger.error(e);
@@ -231,5 +327,37 @@ public class InputFieldHandler {
 
 	public void setNode(SimpleNode node) {
 		this.node = node;
+	}
+
+	public String getRegex() {
+		return regex;
+	}
+
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+
+	public String getRegexValidationText() {
+		return regexValidationText;
+	}
+
+	public void setRegexValidationText(String regexValidationText) {
+		this.regexValidationText = regexValidationText;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public int getMaxlength() {
+		return maxlength;
+	}
+
+	public void setMaxlength(int maxLength) {
+		this.maxlength = maxLength;
 	}
 }
